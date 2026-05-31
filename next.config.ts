362b6886fdce6849be36ba579ca92385c0f47dc8
RChaubey16 <ruturajchaubey16@gmail.com>
2026-05-31 12:20:44 +0530

    fix: build error fixes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    resolveAlias: {
+      tailwindcss: path.resolve(__dirname, "node_modules/tailwindcss"),
+    },
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
